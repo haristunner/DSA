bc4b6d2a86ec6eac9072f6941603b986726cc474
hari <103772225+haristunner@users.noreply.github.com>
2022-10-13 11:44:28 +0530

    Create insert_linked_list.cpp

---
+++ insert_linked_list.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    
+    Node(int d, Node* n){
+        data=d;
+        next=n;
+    }
+};
+void insert(Node* prev, Node* newn)
+    {
+        newn->next=prev->next;
+        prev->next=newn;
+    }
+
+void print(Node* a){
+    while(a!=NULL){
+        cout<<a->data<<" ";
+        a=a->next;
+    }    
+}
+
+int main()
+{
+    Node* p, *q, *r;
+    q=new Node(20,NULL);
+    p=new Node(10,q);
+    r=new Node(30,q);
+    insert(p, r);
+    print(p);
+    return 0;
+}
